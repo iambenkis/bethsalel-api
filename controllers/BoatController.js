e63eeb620560fc874cf8f9916b34b61aa9a08d12
benkis <benkisenge03@gmail.com>
2023-09-12 20:19:31 +0200

    Create boat controller

---
+++ controllers/BoatController.js
@@ -0,0 +1,93 @@
+const Boat = require('../models/Boat');
+
+const index = (req, res, next) => {
+    Boat.find()
+        .then(response => {
+            res.json({
+                response
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: 'An error Occured!'
+            })
+        })
+}
+
+const show = (req, res, next) => {
+    let boatItem = req.body.boatItem
+    Boat.findById(boatItem)
+        .then(response => {
+            res.json({
+                response
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: 'An error Occured!'
+            })
+        })  
+}
+
+const store = (req, res, next) => {
+    let boat = new Boat({
+        name: req.body.name,
+        class: req.body.class,
+        capacity: req.body.capacity,
+        image: req.body.image
+    })
+    boat.save()
+        .then(response => {
+            res.json({
+                message: 'Boat Added Successfully!'
+            })
+        }) 
+        .catch(error => {
+            res.json({
+                message: 'An error Occured!'
+            })
+        })
+}
+
+
+const update = (req, res, next) => {
+    let boatItem = req.body.boatItem
+
+    let updatedData = {
+        name: req.body.name,
+        class: req.body.class,
+        capacity: req.body.capacity
+    }
+
+    Boat.findByIdAndUpdate(boatItem, {$set: updatedData})
+        .then(() => {
+            res.json({
+                message: 'Boat Updated Successfully!'
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: 'An error Occured!'
+            })
+        })
+}
+
+
+const destroy = (req, res, next) => {
+    let boatItem = req.body.boatItem
+    Boat.findByIdAndRemove(boatItem)
+        .then(() => {
+            res.json({
+                message: 'Boat Deleted Successfully!'
+            })
+        })
+        .catch(error => {
+            res.json({
+                message: 'An error Occured!'
+            })
+        })
+}
+
+module.exports = {
+    index, show, store, update, destroy
+}
