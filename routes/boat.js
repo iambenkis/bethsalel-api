ee53f6063543cb2594569ac2ecee61fcca4b857d
benkis <benkisenge03@gmail.com>
2023-09-13 23:55:24 +0200

    Update baot routes

--- routes/boat.js
+++ routes/boat.js
@@ -1,12 +1,13 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-const BoatController = require('../controllers/BoatController');
+const BoatController = require('../controllers/BoatController')
+const upload = require('../middleware/upload')
 
-router.get('/', BoatController.index);
-router.post('/show', BoatController.show);
-router.post('/store', BoatController.store);
-router.post('/update', BoatController.update);
-router.post('/delete', BoatController.destroy);
+router.get('/', BoatController.index)
+router.post('/show', BoatController.show)
+router.post('/store', upload.single('image'), BoatController.store)
+router.post('/update', BoatController.update)
+router.post('/delete', BoatController.destroy)
 
-module.exports = router;
+module.exports = router
