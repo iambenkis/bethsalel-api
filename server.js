f6048dbd76d2bfad47856529dad35f6487105596
benkis <benkisenge03@gmail.com>
2023-09-13 23:53:50 +0200

    Add user in the server

--- server.js
+++ server.js
@@ -1,33 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
-const BoatRoutes = require('./routes/boat');
+const BoatRoutes = require('./routes/boat')
+const AuthRoutes = require('./routes/auth')
 
-const dbUrl = "mongodb+srv://benkis:benkisenge03@cluster0.0ntebcb.mongodb.net/?retryWrites=true&w=majority"
+const dbUrl = process.env.DB_URL
 
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 
 db.on('error', (err) => {
-    console.log(err)
+  console.log(err)
 })
 
 db.once('open', () => {
-    console.log('DB Connection Established!')
+  console.log('DB Connection Established!')
 })
 
-const app = express();
+const app = express()
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use('/uploads', express.static('uploads'))
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`);
+  console.log(`Server is running on PORT ${PORT}`)
 })
 
-app.use('/api/boat', BoatRoutes);
+app.use('/api/boat', BoatRoutes)
+app.use('/api', AuthRoutes)
