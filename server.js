dbde884c6daf03186fc97b6bd70194dbb3d4d917
benkis <benkisenge03@gmail.com>
2023-09-18 23:51:27 +0200

    Add use cors in the server

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const BoatRoutes = require('./routes/boat')
 const AuthRoutes = require('./routes/auth')
-
+const ReservationRoutes = require('./routes/reservation')
 const dbUrl = process.env.DB_URL
 
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -22,6 +23,7 @@ db.once('open', () => {
 
 const app = express()
 
+app.use(cors())
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
@@ -35,3 +37,4 @@ app.listen(PORT, () => {
 
 app.use('/api/boat', BoatRoutes)
 app.use('/api', AuthRoutes)
+app.use('/api', ReservationRoutes)
