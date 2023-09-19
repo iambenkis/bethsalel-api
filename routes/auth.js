4ea1798aa2eba2acc6119333e72f84391ef14edc
benkis <benkisenge03@gmail.com>
2023-09-19 23:53:15 +0200

    Update auth router

--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,6 @@ const User = require('../controllers/AuthController')
 
 router.post('/register', User.register)
 router.post('/login', User.login)
+router.get('/session', User.sessionChecker)
 
 module.exports = router
